9f515ce92ba2fdfd9754b73fcd8fe09fa73c4f49
ramaaldiyansah26 <32946850+ramaaldiyansah26@users.noreply.github.com>
2020-03-12 10:27:47 +0700

    projek

---
+++ resto/confiq/database.php
@@ -0,0 +1,16 @@
+<?php 
+
+	class database{
+		private $db = [
+			'host' => 'localhost',
+			'username' => 'root',
+			'password' => '',
+			'database' => 'resto'
+		];
+
+		public function data(){
+			return $this->db;	
+		}
+	}
+
+?>
